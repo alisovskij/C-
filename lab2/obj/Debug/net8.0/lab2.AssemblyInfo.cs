e674f9bda72a73e1f1e189f34f8f984d56a675be
Artsiom Lisouski <artsiom.lisouski@azati.com>
2025-11-12 18:26:55 +0300

    last changes

--- lab2/obj/Debug/net8.0/lab2.AssemblyInfo.cs
+++ lab2/obj/Debug/net8.0/lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b61bac21e2bfae0cd1e224b0bbd15ec1acc67ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac1f0207f96f2d69d5ab8c0df2f65b434f86448")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
